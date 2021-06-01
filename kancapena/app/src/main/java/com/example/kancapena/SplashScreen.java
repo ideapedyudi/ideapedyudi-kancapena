b5aa12a6df3f44caa779c408230cfe604be48846
Muhammad Wahyudi <ideapedyudi@gmail.com>
2021-06-01 23:30:45 +0700

    gambar

---
+++ kancapena/app/src/main/java/com/example/kancapena/SplashScreen.java
@@ -0,0 +1,31 @@
+package com.example.kancapena;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.widget.TextView;
+
+
+public class SplashScreen extends AppCompatActivity {
+    TextView tvSplash;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //menghilangkan ActionBar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash_screen);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 2000L); //3000 L = 3 detik
+    }
+}
